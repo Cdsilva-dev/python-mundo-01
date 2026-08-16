4ac4d7fbba1e72301d5e8b2fd5993f12288ebdb4
Cdsilva-dev <cdsilvaoliveira840@gmail.com>
2026-08-16 13:23:51 -0300

    Update ex008.py
    adicionando complemento do exercício

--- exercícios_python/ex008.py
+++ exercícios_python/ex008.py
@@ -1,4 +1,8 @@
 numero = float(input("digite o valor em Metros :"))
 centimetros = numero*100
 milimetros = numero*1000
-print(f" O Valor em Metros é : {numero}, em centímetros é : {centimetros} e em milímetros é {milimetros}")
+kilometros = numero/1000
+hectometros = numero/100
+decametros = numero/10
+decimetros = numero*10
+print(f"O Valor em Metro é {numero}\n {kilometros}km \n {hectometros}hm \n {decametros}dam \n {decimetros}dm \n {centimetros} cm \n {milimetros}mm \n")
