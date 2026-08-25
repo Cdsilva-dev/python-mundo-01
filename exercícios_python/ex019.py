29b1f79ecceb0c65f76cf7d7142debbf9444b0b4
Cdsilva-dev <cdsilvaoliveira840@gmail.com>
2026-08-25 00:27:41 -0300

    ajustando o exercício 19

--- exercícios_python/ex019.py
+++ exercícios_python/ex019.py
@@ -1,6 +1,6 @@
 import random
-Nomes = (input("Digite o Primeiro nome "))
+Nome = input("Digite o Primeiro nome ")
 Segundo_nome = input("Digite o Segundo nome ")
 Terceiro_nome = input("Digite o Terceiro nome ")
 Quarto_nome =input("Digite o Quarto nome ")
-print(f"o aluno escolhido foi : {random.choice()}")
+print(f"o aluno escolhido foi : {random.choice([Nome, Segundo_nome, Terceiro_nome, Quarto_nome])}")
